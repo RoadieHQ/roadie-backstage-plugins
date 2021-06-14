ebf277cdd8d1f43e65cdea6f79c3960a98c47ad2
Irma12 <irma@roadie.io>
2021-06-14 16:01:23 +0200

    Fixtures for github insights

--- packages/app/cypress/integration/github-insights.ts
+++ packages/app/cypress/integration/github-insights.ts
@@ -20,12 +20,30 @@ import 'os';
 describe('GithubInsights', () => {
   beforeEach(() => {
     cy.saveGithubToken();
+    cy.intercept('GET', 'https://api.github.com/repos/organisation/github-project-slug/languages', { fixture: 'githubInsights/languages.json' })
+    cy.intercept('GET', 'https://api.github.com/repos/organisation/github-project-slug/releases', { fixture: 'githubInsights/releases.json' })
+    cy.intercept('GET', 'https://api.github.com/repos/organisation/github-project-slug/readme', { fixture: 'githubInsights/readme.json' })
+    cy.intercept('GET', 'https://api.github.com/repos/organisation/github-project-slug/branches?protected=true', { fixture: 'githubInsights/compliance.json' })
+    cy.intercept('GET', 'https://api.github.com/repos/organisation/github-project-slug/contributors?per_page=10', { fixture: 'githubInsights/contributors.json' })
+    cy.visit('/catalog/default/component/sample-service')
   })
   
   describe('Navigating to GitHub Insights', () => {
-    it('should show GitHub Insights Releases widget in Overview tab', () => {
-      cy.visit('/catalog/default/component/sample-service')
+    it('should show GitHub Insights Releases in Overview tab', () => {
       cy.contains('Releases');
     });
+
+    it('should show GitHub Insights Languages in Overview tab', () => {
+      cy.contains('Languages');
+    });
+
+    it('should show GitHub Insights Read me in Overview tab', () => {
+      cy.contains('Read me');
+    });
+
+    it('should show GitHub Insights when navigating to Code insights tab', () => {
+      cy.visit('/catalog/default/component/sample-service/code-insights')
+      cy.contains('GitHub Insights');
+    });
    });
-});
+  });
