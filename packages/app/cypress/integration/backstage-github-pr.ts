b16ece4188fa0343c49f2a650fad4e2d9c5ee8c8
Irma12 <irma@roadie.io>
2021-06-14 17:00:58 +0200

    Fixtures for GithubPR plugin

--- packages/app/cypress/integration/backstage-github-pr.ts
+++ packages/app/cypress/integration/backstage-github-pr.ts
@@ -16,3 +16,22 @@
 /// <reference types="cypress" />
 // eslint-disable-next-line no-restricted-imports
 import 'os';
+
+describe('Github Pull Requests', () => {
+    beforeEach(() => {
+        cy.saveGithubToken();
+        cy.intercept('GET', 'https://api.github.com/repos/organisation/github-project-slug/pulls?state=open&per_page=5&page=1', { fixture: 'githubPRs/pull-requests.json' })
+        cy.visit('/catalog/default/component/sample-service')
+    })
+
+    describe('Navigating to GitHub Pull Requests', () => {
+        it('should show GitHub Pull Requests in Overview tab', () => {
+            cy.contains('Pull requests statistics');
+        });
+
+        it('should show GitHub Pull Requests when navigating to Pull Requests tab', () => {
+            cy.visit('/catalog/default/component/sample-service/pull-requests')
+            cy.contains('Pull requests plugin');
+          });
+    });
+});
