20fe984543d7d3f34728c1a3e9096514fc1be9ca
Irma12 <irma@roadie.io>
2021-06-16 10:16:26 +0200

    rename fixture repo for datadog

--- packages/app/cypress/integration/datadog.ts
+++ packages/app/cypress/integration/datadog.ts
@@ -20,8 +20,8 @@ import 'os';
 describe('Datadog', () => {
     beforeEach(() => {
         cy.saveGithubToken();
-        cy.intercept('GET', 'https://p.datadoghq.eu/sb/test-datadog-link', { fixture: 'Datadog/datadogdashboard.json' })
-        cy.intercept('GET', 'https://app.datadoghq.eu/graph/embed?token=qwertytoken1234&height=300&width=600&legend=true', { fixture: 'Datadog/dashboard.html' })
+        cy.intercept('GET', 'https://p.datadoghq.eu/sb/test-datadog-link', { fixture: 'datadog/datadogdashboard.json' })
+        cy.intercept('GET', 'https://app.datadoghq.eu/graph/embed?token=qwertytoken1234&height=300&width=600&legend=true', { fixture: 'datadog/dashboard.html' })
         cy.visit('/catalog/default/component/sample-service')
     })
 
