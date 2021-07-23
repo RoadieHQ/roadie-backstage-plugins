22531232dfabf0fb7c02c71c8daa5ade0476e6f4
irma12 <irma@roadie.io>
2021-07-23 13:29:32 +0200

    Fix e2e test

--- packages/app/cypress/integration/AWS-lambda.ts
+++ packages/app/cypress/integration/AWS-lambda.ts
@@ -20,13 +20,13 @@ import 'os';
 describe('AWS Lambda', () => {
     beforeEach(() => {
         cy.saveGithubToken();
-        cy.intercept('GET', 'http://localhost:7000/api/aws/credentials', { fixture: 'AWSLambda/AWScredentials.json' })
+        cy.intercept('GET', 'http://localhost:7000/api/aws/credentials', { fixture: 'AWSLambda/AWSCredentials.json' })
         cy.intercept('GET', 'https://lambda.eu-west-1.amazonaws.com/2015-03-31/functions/HelloWorld', { fixture: 'AWSLambda/AWSLambdaResponse.json' })
         cy.visit('/catalog/default/component/sample-service')
     })
 
     describe('Navigating to AWS', () => {
-        it('should show AWS', () => {
+        it('should show HelloWorld', () => {
             cy.contains('HelloWorld');
         });
     });
