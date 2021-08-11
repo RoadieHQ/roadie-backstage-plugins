1d5d0f2dc37b659b861bc0cb3972049e94dc7f0f
Iain Billett <iain@roadie.io>
2021-08-11 15:18:59 +0100

    Add integration tests for ArgoCD

---
+++ packages/app/cypress/integration/ArgoCD.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 Larder Software Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/// <reference types="cypress" />
+// eslint-disable-next-line no-restricted-imports
+import 'os';
+
+describe('Travis CI', () => {
+  beforeEach(() => {
+    cy.saveGithubToken();
+    cy.intercept(
+      'GET',
+      'http://localhost:7000/api/proxy/argocd/api/applications/test-app',
+      { fixture: 'ArgoCD/applications-test-app.json' },
+    );
+    cy.visit('/catalog/default/component/sample-service');
+  });
+
+  describe('Navigate to Overview', () => {
+    it('should show the ArgoCD History card', () => {
+      cy.visit('/catalog/default/component/sample-service');
+      cy.contains('ArgoCD history');
+      cy.contains('test-app');
+      cy.contains('53e28ff20cc530b9ada2173fbbd64d48338583ba');
+    });
+
+    it('should show ArgoCD app details card', () => {
+      cy.visit('/catalog/default/component/sample-service');
+      cy.contains('ArgoCD overview');
+      cy.contains('test-app');
+      cy.contains('Synced');
+      cy.contains('Healthy');
+    });
+  });
+
+  describe('Navigate to argocd tab', () => {
+    it('should show the ArgoCD History table', () => {
+      cy.visit('/catalog/default/component/sample-service/argocd');
+      cy.contains('ArgoCD history');
+      cy.contains('test-app');
+      cy.contains('53e28ff20cc530b9ada2173fbbd64d48338583ba');
+    });
+  });
+});
