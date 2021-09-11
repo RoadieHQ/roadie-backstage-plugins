6c66c78e98ba5970751b0c49debd1f155e61e23a
irma12 <irma@roadie.io>
2021-09-11 22:30:59 +0200

    minor fix

--- plugins/backstage-plugin-argo-cd/src/components/ArgoCDHistoryCard.tsx
+++ plugins/backstage-plugin-argo-cd/src/components/ArgoCDHistoryCard.tsx
@@ -58,88 +58,89 @@ const HistoryTable = ({
           });
         }
         return {};
-      }).flat()
+      }).filter(value => Object.keys(value).length !== 0)
+      .flat()
     : [];
 
-const columns: TableColumn[] = [
-  {
-    title: 'Name',
-    field: 'name',
-    render: (row: any): React.ReactNode =>
-      baseUrl ? (
-        <Link
-          href={`${baseUrl}/applications/${row.app}`}
-          target="_blank"
-          rel="noopener"
+  const columns: TableColumn[] = [
+    {
+      title: 'Name',
+      field: 'name',
+      render: (row: any): React.ReactNode =>
+        baseUrl ? (
+          <Link
+            href={`${baseUrl}/applications/${row.app}`}
+            target="_blank"
+            rel="noopener"
+          >
+            {row.app}
+          </Link>
+        ) : (
+          row.app
+        ),
+      highlight: true,
+    },
+    {
+      title: 'Deploy Started',
+      field: 'deployStartedAt',
+      render: (row: any) => (
+        <Tooltip
+          title={
+            row.deployStartedAt ? row.deployStartedAt : 'Deploy started at'
+          }
+          placement="left"
         >
-          {row.app}
-        </Link>
-      ) : (
-        row.app
+          <div>
+            {row.deployStartedAt ? moment(row.deployStartedAt).fromNow() : '-'}
+          </div>
+        </Tooltip>
       ),
-    highlight: true,
-  },
-  {
-    title: 'Deploy Started',
-    field: 'deployStartedAt',
-    render: (row: any) => (
-      <Tooltip
-        title={
-          row.deployStartedAt ? row.deployStartedAt : 'Deploy started at'
-        }
-        placement="left"
-      >
-        <div>
-          {row.deployStartedAt ? moment(row.deployStartedAt).fromNow() : '-'}
-        </div>
-      </Tooltip>
-    ),
-  },
-  {
-    title: 'Deployed At',
-    field: 'deployedAt',
-    render: (row: any) => (
-      <Tooltip title={row.deployedAt} placement="left">
-        <div>{moment(row.deployedAt).fromNow()}</div>
-      </Tooltip>
-    ),
-  },
-  {
-    title: 'Deploy duration',
-    render: (row: any): React.ReactNode =>
-      moment
-        .duration(moment(row.deployStartedAt).diff(moment(row.deployedAt)))
-        .humanize(),
-    sorting: false,
-  },
-  {
-    title: 'Revision',
-    field: 'revision',
-    sorting: false,
-  },
-];
+    },
+    {
+      title: 'Deployed At',
+      field: 'deployedAt',
+      render: (row: any) => (
+        <Tooltip title={row.deployedAt} placement="left">
+          <div>{moment(row.deployedAt).fromNow()}</div>
+        </Tooltip>
+      ),
+    },
+    {
+      title: 'Deploy duration',
+      render: (row: any): React.ReactNode =>
+        moment
+          .duration(moment(row.deployStartedAt).diff(moment(row.deployedAt)))
+          .humanize(),
+      sorting: false,
+    },
+    {
+      title: 'Revision',
+      field: 'revision',
+      sorting: false,
+    },
+  ];
 
-return (
-  <Table
-    title="ArgoCD history"
-    options={{
-      paging: false,
-      search: false,
-      draggable: false,
-      padding: 'dense',
-    }}
-    data={history}
-    columns={columns}
-    actions={[
-      {
-        icon: () => <SyncIcon />,
-        tooltip: 'Refresh',
-        isFreeAction: true,
-        onClick: () => retry(),
-      },
-    ]}
-  />
-);
+  return (
+    <Table
+      title="ArgoCD history"
+      options={{
+        paging: false,
+        search: false,
+        draggable: false,
+        padding: 'dense',
+      }}
+      data={history}
+      columns={columns}
+      actions={[
+        {
+          icon: () => <SyncIcon />,
+          tooltip: 'Refresh',
+          isFreeAction: true,
+          onClick: () => retry(),
+        },
+      ]}
+    />
+  );
 };
 
 const ArgoCDHistory = ({ entity }: { entity: Entity }) => {
