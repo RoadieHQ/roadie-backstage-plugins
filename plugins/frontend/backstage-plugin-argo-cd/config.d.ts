7c1bb887156da41f1b889880fabf75e407737dc3
Karl Haworth <karl.haworth@aa.com>
2021-11-04 09:41:51 -0400

    remove items from  that are no longer needed on the frontend

--- plugins/frontend/backstage-plugin-argo-cd/config.d.ts
+++ plugins/frontend/backstage-plugin-argo-cd/config.d.ts
@@ -10,25 +10,6 @@ export interface Config {
      * The base url of the ArgoCD instance.
      * @visibility frontend
      */
-    appLocatorMethods?: Array</**
-     * @visibility secret
-     */
-    {
-      /**
-       * The base url of the ArgoCD instance.
-       * @visibility frontend
-       */
-      type: string;
-      instances: Array<{
-        /**
-         * @visibility frontend
-         */
-        name: string;
-        /**
-         * @visibility frontend
-         */
-        url: string;
-      }>;
-    }>;
+    appLocatorMethods?: Array<object>;
   };
 }
