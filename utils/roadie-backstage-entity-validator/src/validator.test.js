212fb221351489961bef3d11102e4dcb26e7faf4
Nicolas Arnold <nic@roadie.io>
2021-11-29 17:21:26 +0000

    Fixing test and bumping to a new version

--- utils/roadie-backstage-entity-validator/src/validator.test.js
+++ utils/roadie-backstage-entity-validator/src/validator.test.js
@@ -14,7 +14,7 @@ describe('validator',  () => {
   });
 
   it('Should fail to validate with incorrect catalog-info that has an empty label', async () => {
-    await expect(validator.validateFromFile('./sample/catalog-info-with-empty-label.yml')).rejects.toThrow("Error: Placeholder with name definition is empty. Please remove it or populate it.");
+    await expect(validator.validateFromFile('./sample/catalog-info-with-empty-label.yml')).rejects.toThrow("Error: Placeholder with name 'definition' is empty. Please remove it or populate it.");
   });
 })
 
