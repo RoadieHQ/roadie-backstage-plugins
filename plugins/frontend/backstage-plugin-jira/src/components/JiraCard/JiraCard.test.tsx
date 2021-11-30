5475934547ad59e206ae25d1b7570aa1c653f804
irma12 <irma@roadie.io>
2021-11-30 09:24:47 +0100

    re-add existing tests

--- plugins/frontend/backstage-plugin-jira/src/components/JiraCard/JiraCard.test.tsx
+++ plugins/frontend/backstage-plugin-jira/src/components/JiraCard/JiraCard.test.tsx
@@ -16,10 +16,13 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import { ApiProvider, ApiRegistry, UrlPatternDiscovery } from '@backstage/core-app-api';
-import { EntityProvider } from "@backstage/plugin-catalog-react";
+import { ApiProvider, UrlPatternDiscovery } from '@backstage/core-app-api';
+import { EntityProvider } from '@backstage/plugin-catalog-react';
 import { rest } from 'msw';
-import { msw } from '@backstage/test-utils';
+import {
+  setupRequestMockHandlers,
+  TestApiRegistry,
+} from '@backstage/test-utils';
 import { setupServer } from 'msw/node';
 // eslint-disable-next-line
 import { MemoryRouter } from 'react-router-dom';
@@ -35,13 +38,14 @@ import {
 
 const discoveryApi = UrlPatternDiscovery.compile('http://exampleapi.com');
 
-const apis = ApiRegistry.from([[jiraApiRef, new JiraAPI({ discoveryApi })]]);
+const apis = TestApiRegistry.from([jiraApiRef, new JiraAPI({ discoveryApi })]);
 
 describe('JiraCard', () => {
   const worker = setupServer();
-  msw.setupDefaultHandlers(worker);
+  setupRequestMockHandlers(worker);
 
   beforeEach(() => {
+    worker.resetHandlers();
     jest.resetAllMocks();
   });
 
@@ -74,7 +78,20 @@ describe('JiraCard', () => {
       </MemoryRouter>,
     );
 
-    expect(await rendered.findByText("ActivityStream")).toBeInTheDocument();
+    expect(await rendered.findByText(/backstage-test/)).toBeInTheDocument();
+    expect(
+      (await rendered.findAllByText(/testComponent/)).length,
+    ).toBeGreaterThan(0);
+    expect(
+      await rendered.findByText(
+        /changed the status to Selected for Development/,
+      ),
+    ).toBeInTheDocument();
+    expect(await rendered.findByText(/Add basic test/)).toBeInTheDocument();
+    expect(await rendered.getByAltText(/Page/)).toHaveAttribute(
+      'src',
+      expect.stringContaining('mocked_icon_filename.gif'),
+    );
   });
 
   it('should display an error on fetch failure', async () => {
@@ -105,7 +122,7 @@ describe('JiraCard', () => {
       </MemoryRouter>,
     );
 
-    const text = await rendered.findByText(/status 403: Forbidden/)
-      expect(text).toBeInTheDocument();
+    const text = await rendered.findByText(/status 403: Forbidden/);
+    expect(text).toBeInTheDocument();
   });
 });
