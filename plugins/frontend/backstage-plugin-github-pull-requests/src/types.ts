3cedd64257a50a6e69c89f2bee163a6378306346
fpernin <frederic.pernin@outlook.com>
2022-01-11 15:28:04 +0100

    feat: adding type for items in response data for search results

--- plugins/frontend/backstage-plugin-github-pull-requests/src/types.ts
+++ plugins/frontend/backstage-plugin-github-pull-requests/src/types.ts
@@ -1,60 +1,18 @@
+import { SearchIssuesAndPullRequestsResponseData } from '@octokit/types';
+
 export type PullRequestState = 'open' | 'closed' | 'all';
 
-export type SearchPullRequestsResponseData = {
-    total_count: number;
-    incomplete_results: boolean;
-    items: {
-        url: string;
-        repository_url: string;
-        labels_url: string;
-        comments_url: string;
-        events_url: string;
+export type SearchPulLRequestsResponseDataItems = {
+  items: Array<
+    Omit<SearchIssuesAndPullRequestsResponseData['items'][number], "pull_request"> & {
+      draft:boolean;
+      pull_request: {
         html_url: string;
-        id: number;
-        node_id: string;
-        number: number;
-        title: string;
-        user: {
-            login: string;
-            id: number;
-            node_id: string;
-            avatar_url: string;
-            gravatar_id: string;
-            url: string;
-            html_url: string;
-            followers_url: string;
-            following_url: string;
-            gists_url: string;
-            starred_url: string;
-            subscriptions_url: string;
-            organizations_url: string;
-            repos_url: string;
-            events_url: string;
-            received_events_url: string;
-            type: string;
-        };
-        labels: {
-            id: number;
-            node_id: string;
-            url: string;
-            name: string;
-            color: string;
-        }[];
-        state: string;
-        assignee: string;
-        milestone: string;
-        comments: number;
-        created_at: string;
-        updated_at: string;
-        closed_at: string;
-        draft: boolean;
-        pull_request: {
-            html_url: string;
-            diff_url: string;
-            patch_url: string;
-            merged_at: string | null;
-        };
-        body: string;
-        score: number;
-    }[];
+        diff_url: string;
+        patch_url: string;
+        merged_at: string | null;
+      };
+    }>
 }
+
+export type SearchPullRequestsResponseData = Omit<SearchIssuesAndPullRequestsResponseData, "items"> &  SearchPulLRequestsResponseDataItems;
