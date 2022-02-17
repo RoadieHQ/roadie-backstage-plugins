6bd957a187ddc4d72703db77beea68af3d99af89
Kiss Miklos <miklos@roadie.io>
2022-02-17 17:03:44 +0100

    try to wait for the request

--- packages/app/cypress/integration/ArgoCD.ts
+++ packages/app/cypress/integration/ArgoCD.ts
@@ -31,6 +31,7 @@ describe('Argo CD', () => {
 
   describe('Navigate to Overview', () => {
     it('should show the ArgoCD History card', () => {
+      cy.wait('@getArgoData')
       cy.contains('ArgoCD history');
       cy.contains('test-app');
       cy.contains('53e28ff20cc530b9ada2173fbbd64d48338583ba');
