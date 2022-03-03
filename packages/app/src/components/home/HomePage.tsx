804f5d055c29d0607844245f1646767626113ebc
Kiss Miklos <miklos@roadie.io>
2022-03-03 13:03:27 +0100

    Add homepage plugin for example app

---
+++ packages/app/src/components/home/HomePage.tsx
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Larder Software Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { HomePageToolkit } from '@backstage/plugin-home';
+
+export const HomePage = () => {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={4}>
+        <HomePageToolkit
+          tools={[
+            {
+              label: 'Backstage',
+              url: 'https://github.com/backstage/backstage',
+              icon: <></>,
+            },
+          ]}
+        />
+      </Grid>
+    </Grid>
+  );
+};
