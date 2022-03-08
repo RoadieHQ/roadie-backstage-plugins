8fd879b651f485a041c8391d1d854aaa01bcb6d9
Kiss Miklos <miklos@roadie.io>
2022-03-08 15:21:14 +0100

    use the correct notice header

--- plugins/frontend/backstage-plugin-security-insights/src/components/SecurityInsightsTable/components/StateFilterComponent/index.ts
+++ plugins/frontend/backstage-plugin-security-insights/src/components/SecurityInsightsTable/components/StateFilterComponent/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 RoadieHQ
+ * Copyright 2021 Larder Software Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,5 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 export { StateFilterComponent } from './StateFilterComponent';
--- plugins/frontend/backstage-plugin-security-insights/src/components/SecurityInsightsTable/index.ts
+++ plugins/frontend/backstage-plugin-security-insights/src/components/SecurityInsightsTable/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 RoadieHQ
+ * Copyright 2021 Larder Software Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,4 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 export { SecurityInsightsTable } from './SecurityInsightsTable';
