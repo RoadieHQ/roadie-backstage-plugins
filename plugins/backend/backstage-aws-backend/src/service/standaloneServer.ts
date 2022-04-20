b207e9f60aa1538b4d460c618cd77263a65489f1
Brian Fletcher <brian@roadie.io>
2022-04-20 20:19:53 +0100

    adds license

--- plugins/backend/backstage-aws-backend/src/service/standaloneServer.ts
+++ plugins/backend/backstage-aws-backend/src/service/standaloneServer.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Larder Software Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {
   createServiceBuilder,
   loadBackendConfig,
@@ -16,7 +32,7 @@ export async function startStandaloneServer(
   options: ServerOptions,
 ): Promise<Server> {
   const logger = options.logger.child({
-    service: 'backstage-plugin-argo-cd-backend',
+    service: 'backstage-aws-backend',
   });
   logger.debug('Starting application server...');
 
@@ -32,7 +48,7 @@ export async function startStandaloneServer(
 
   const service = createServiceBuilder(module)
     .enableCors({ origin: 'http://localhost:7007' })
-    .addRouter('/backstage-plugin-argo-cd-backend', router);
+    .addRouter('/backstage-aws-backend', router);
 
   return await service.start().catch(err => {
     logger.error(err);
