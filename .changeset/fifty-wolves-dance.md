1f87336702001ef9d0bfbc228501172deec6539b
Brian Fletcher <brian@roadie.io>
2022-11-08 19:56:10 +0000

    export markdown and rss plugins from their packages

---
+++ .changeset/fifty-wolves-dance.md
@@ -0,0 +1,5 @@
+---
+'@roadiehq/backstage-plugin-home-rss': patch
+---
+
+Export the rss plugin from the package
--- plugins/home/backstage-plugin-home-markdown/src/index.ts
+++ plugins/home/backstage-plugin-home-markdown/src/index.ts
@@ -14,4 +14,4 @@
  * limitations under the License.
  */
 
-export { HomePageMarkdown } from './plugin';
+export { HomePageMarkdown, markdownPlugin } from './plugin';
