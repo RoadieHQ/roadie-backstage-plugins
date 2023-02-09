13939fdaf8c7506e4999c38b32e8c8019336ae5f
sbellam1187 <sudhakarrao.bellam@aa.com>
2023-02-09 13:18:16 -0700

    added the tests

--- plugins/backend/backstage-plugin-argo-cd-backend/src/service/router.test.ts
+++ plugins/backend/backstage-plugin-argo-cd-backend/src/service/router.test.ts
@@ -8,7 +8,27 @@ import {
   argocdCreateApplicationResp,
   argocdCreateProjectResp,
 } from './argocdTestResponses';
-
+const mockDeleteApp = jest.fn();
+const mockDeleteProject = jest.fn();
+const mockGetArgoAppData =jest.fn();
+const mockGetArgoToken = jest.fn();
+const mockCreateArgoProject = jest.fn();
+const mockCreateArgoApplication = jest.fn();
+jest.mock('./argocd.service', () => {
+  return {
+    ArgoService: jest.fn().mockImplementation(() => {
+      return {
+        deleteApp: mockDeleteApp,
+        deleteProject: mockDeleteProject, 
+        getArgoAppData: mockGetArgoAppData,
+        getArgoToken: mockGetArgoToken,
+        createArgoProject: mockCreateArgoProject,
+        createArgoApplication: mockCreateArgoApplication,
+        
+      };
+    }),
+  };
+});
 const logger = getVoidLogger();
 const config = ConfigReader.fromConfigs([
   {
@@ -24,24 +44,28 @@ const config = ConfigReader.fromConfigs([
               {
                 name: 'argoInstance1',
                 url: 'https://argoInstance1.com',
+                username: 'testusername',
+                password: 'testpassword',
+                token: 'token'
               },
             ],
           },
         ],
+        waitCycles: 5
       },
     },
   },
 ]);
-
 describe('router', () => {
   let app: Express;
-
   beforeEach(async () => {
     const router = await createRouter({ config, logger });
     app = express().use(router);
     fetchMock.resetMocks();
+    mockDeleteProject.mockReset();
+    mockDeleteApp.mockReset();
+    mockGetArgoAppData.mockReset();
   });
-
   it('returns the requested data', async () => {
     fetchMock.mockOnceIf(
       /.*\/api\/v1\/session/g,
@@ -70,6 +94,159 @@ describe('router', () => {
       argoAppName: 'test-app-nonprod',
       argoProjectName: 'test-project',
       kubernetesNamespace: 'test-namespace',
+    });
+  });
+  it('delete fails when argo instance is not found', async () => {
+    fetchMock.mockOnceIf(
+      /.*\/api\/v1\/session/g,
+      JSON.stringify({ token: 'testToken' }),
+    );
+    const response = await request(app).delete('/argoInstance/:argoInstanceName/applications/argoInstance2');
+    expect(response.body).toMatchObject({
+      status: 'failed',
+      message: 'cannot find an argo instance to match this cluster',
+    });
+  });
+  it('when deleteApp returns 404 Not found continue to delete Project', async () => {
+    mockDeleteApp.mockRejectedValueOnce(new Error('Not Found'));
+    mockDeleteProject.mockResolvedValueOnce(true);
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'failed',
+        message: 'Not Found'
+      },
+      argoDeleteProjectResp: {
+        status: 'success',
+        message: 'project is deleted successfully'
+      },
+    });
+  });
+  it('when deleteApp gives 5xx errors skip project deletion', async () => {
+    mockDeleteApp.mockResolvedValueOnce(false);
+    mockDeleteProject.mockResolvedValueOnce(true);
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'failed',
+        message: 'error with deleteing argo app'
+      },
+      argoDeleteProjectResp: {
+        status: 'failed',
+        message: 'skipping project deletion due to erro deleting argo app'
+      },
+    });
+  });
+  it('when app is in pending to delete state skip project deletion', async () => {
+    mockDeleteApp.mockResolvedValueOnce(true);
+    mockGetArgoAppData.mockResolvedValue({
+      metadata: {
+        instance: {
+          name: 'argoInstance1'
+        }
+      }
+    })
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'failed',
+        message: 'application pending delete'
+      },
+      argoDeleteProjectResp: {
+        status: 'failed',
+        message: 'skipping project deletion due to app deletion pending'
+      },
+    });
+  });
+  it('when getArgoCD returns 404 every time and app is in pending to delete state skip project deletion', async () => {
+    mockDeleteApp.mockResolvedValueOnce(true);
+    mockGetArgoAppData.mockResolvedValueOnce({
+      metadata: {
+        instance: {
+          name: 'argoInstance1'
+        }
+      }
+    })
+    mockGetArgoAppData.mockRejectedValue(new Error('Could not retrieve ArgoCD app data.'))
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'failed',
+        message: 'application pending delete'
+      },
+      argoDeleteProjectResp: {
+        status: 'failed',
+        message: 'skipping project deletion due to app deletion pending'
+      },
+    });
+  });
+  it('when getArgoCD returns 404 one occurrence and the app is later deleted then delete project', async () => {
+    mockDeleteApp.mockResolvedValueOnce(true);
+    mockGetArgoAppData.mockResolvedValueOnce({
+      metadata: {
+        instance: {
+          name: 'argoInstance1'
+        }
+      }
+    })
+    mockGetArgoAppData.mockRejectedValueOnce(new Error('Could not retrieve ArgoCD app data.'))
+    mockGetArgoAppData.mockResolvedValueOnce({})
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'success',
+        message: 'application is deleted successfully'
+      },
+      argoDeleteProjectResp: {
+        status: 'success',
+        message: 'project is deleted successfully'
+      },
+    });
+  });
+  it('successfully deletes app and successfuly deletes project', async () => {
+    mockDeleteApp.mockResolvedValueOnce(true);
+    mockGetArgoAppData.mockResolvedValueOnce({
+      metadata: {
+        instance: {
+          name: 'argoInstance1'
+        }
+      }
+    })
+    mockGetArgoAppData.mockResolvedValueOnce({})
+    mockDeleteProject.mockResolvedValueOnce(true);
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'success',
+        message: 'application is deleted successfully'
+      },
+      argoDeleteProjectResp: {
+        status: 'success',
+        message: 'project is deleted successfully'
+      },
+    });
+  });
+  it('succesfully deletes app and fails to delete project', async () => {
+    mockDeleteApp.mockResolvedValueOnce(true);
+    mockGetArgoAppData.mockResolvedValueOnce({
+      metadata: {
+        instance: {
+          name: 'argoInstance1'
+        }
+      }
+    })
+    mockGetArgoAppData.mockResolvedValueOnce({})
+    mockDeleteProject.mockRejectedValueOnce({ message: 'error deleting project'});
+    const response = await request(app).delete('/argoInstance/argoInstance1/applications/appName');
+    expect(response.body).toMatchObject({
+      argoDeleteAppResp:  {
+        status: 'success',
+        message: 'application is deleted successfully'
+      },
+      argoDeleteProjectResp: {
+        status: 'failed',
+        message: 'error deleting project'
+      },
     });
   });
 });
--- plugins/backend/backstage-plugin-argo-cd-backend/src/service/router.ts
+++ plugins/backend/backstage-plugin-argo-cd-backend/src/service/router.ts
@@ -4,17 +4,14 @@ import express from 'express';
 import Router from 'express-promise-router';
 import { Logger } from 'winston';
 import { ArgoService } from './argocd.service';
-
 export interface RouterOptions {
   logger: Logger;
   config: Config;
 }
-
 export type Response = {
   status: string
   message: string
 }
-
 export function createRouter({
   logger,
   config,
@@ -27,15 +24,12 @@ export function createRouter({
     config.getOptionalString('argocd.password') ?? 'argocdPassword';
   const argoWaitCycles: number =
     config.getOptionalNumber('argocd.waitCycles') ?? 5;
-
   const argoSvc = new ArgoService(argoUserName, argoPassword, config);
-
   const argoApps = config
     .getConfigArray('argocd.appLocatorMethods')
     .filter(element => element.getString('type') === 'config');
   const appArray: Config[] = argoApps.reduce(
-    (acc: Config[], argoApp: Config) =>
-      acc.concat(argoApp.getConfigArray('instances')),
+    (acc: Config[], argoApp: Config) =>      acc.concat(argoApp.getConfigArray('instances')),
     [],
   );
   const argoInstanceArray = appArray.map(instance => ({
@@ -45,19 +39,16 @@ export function createRouter({
     username: instance.getOptionalString('username'),
     password: instance.getOptionalString('password'),
   }));
-
   router.get('/find/name/:argoAppName', async (request, response) => {
     const argoAppName = request.params.argoAppName;
     response.send(await argoSvc.findArgoApp({ name: argoAppName }));
   });
-
   router.get(
     '/argoInstance/:argoInstanceName/applications/name/:argoAppName',
     async (request, response) => {
       const argoInstanceName = request.params.argoInstanceName;
       const argoAppName = request.params.argoAppName;
       logger.info(`Getting info on ${argoAppName}`);
-
       logger.info(`Getting app ${argoAppName} on ${argoInstanceName}`);
       const matchedArgoInstance = argoInstanceArray.find(
         argoInstance => argoInstance.name === argoInstanceName,
@@ -68,7 +59,6 @@ export function createRouter({
           message: 'cannot find an argo instance to match this cluster',
         });
       }
-
       let token: string;
       if (!matchedArgoInstance.token) {
         token = await argoSvc.getArgoToken(matchedArgoInstance);
@@ -84,18 +74,15 @@ export function createRouter({
       return response.send(resp);
     },
   );
-
   router.get('/find/selector/:argoAppSelector', async (request, response) => {
     const argoAppSelector = request.params.argoAppSelector;
     response.send(await argoSvc.findArgoApp({ selector: argoAppSelector }));
   });
-
   router.get(
     '/argoInstance/:argoInstanceName/applications/selector/:argoAppSelector',
     async (request, response) => {
       const argoInstanceName = request.params.argoInstanceName;
       const argoAppSelector = request.params.argoAppSelector;
-
       logger.info(
         `Getting apps for selector ${argoAppSelector} on ${argoInstanceName}`,
       );
@@ -108,7 +95,6 @@ export function createRouter({
           message: 'cannot find an argo instance to match this cluster',
         });
       }
-
       let token: string;
       if (!matchedArgoInstance.token) {
         token = await argoSvc.getArgoToken(matchedArgoInstance);
@@ -124,7 +110,6 @@ export function createRouter({
       return response.send(resp);
     },
   );
-
   router.post('/createArgo', async (request, response) => {
     const argoInstanceName = request.body.clusterName;
     const namespace = request.body.namespace;
@@ -133,7 +118,6 @@ export function createRouter({
     const labelValue = request.body.labelValue as string;
     const sourceRepo = request.body.sourceRepo;
     const sourcePath = request.body.sourcePath;
-
     const matchedArgoInstance = argoInstanceArray.find(
       argoInstance => argoInstance.name === argoInstanceName,
     );
@@ -143,7 +127,6 @@ export function createRouter({
         message: 'cannot find an argo instance to match this cluster',
       });
     }
-
     let token: string;
     if (!matchedArgoInstance.token) {
       try {
@@ -157,7 +140,6 @@ export function createRouter({
     } else {
       token = matchedArgoInstance.token;
     }
-
     let argoProjResp = {};
     try {
       argoProjResp = await argoSvc.createArgoProject({
@@ -174,7 +156,6 @@ export function createRouter({
         message: e.message || 'Failed to create argo project',
       });
     }
-
     let argoAppResp = {};
     try {
       argoAppResp = await argoSvc.createArgoApplication({
@@ -200,12 +181,10 @@ export function createRouter({
       });
     }
   });
-
   router.post('/sync', async (request, response) => {
     const appSelector = request.body.appSelector;
     try {
       const argoSyncResp = await argoSvc.resyncAppOnAllArgos({ appSelector });
-
       return response.send(argoSyncResp);
     } catch (e: any) {
       return response.status(e.status || 500).send({
@@ -214,14 +193,12 @@ export function createRouter({
       });
     }
   });
-
   router.delete(
     '/argoInstance/:argoInstanceName/applications/:argoAppName',
     async (request, response) => {
       const argoInstanceName = request.params.argoInstanceName;
       const argoAppName = request.params.argoAppName;
       logger.info(`Getting info on ${argoInstanceName} and ${argoAppName}`);
-
       const matchedArgoInstance = argoInstanceArray.find(
         argoInstance => argoInstance.name === argoInstanceName,
       );
@@ -231,38 +208,46 @@ export function createRouter({
           message: 'cannot find an argo instance to match this cluster',
         });
       }
-
       let token: string;
       if (!matchedArgoInstance.token) {
         token = await argoSvc.getArgoToken(matchedArgoInstance);
       } else {
         token = matchedArgoInstance.token;
       }
-
-      let argoDeleteAppResp: Response;
+      const argoDeleteProjectResp: Response = { 
+        status: '',
+        message: ''
+      };
+      const argoDeleteAppResp: Response = { 
+        status: '',
+        message: ''
+      };
+      let countinueToDeleteProject: boolean = true
       let isAppexist: boolean = true
       try {
-        await argoSvc.deleteApp({
+        const deleteAppResp = await argoSvc.deleteApp({
           baseUrl: matchedArgoInstance.url,
           argoApplicationName: argoAppName,
           argoToken: token,
         });
-      } catch (e: any) {
+        if (deleteAppResp === false) {
+          countinueToDeleteProject = false
+          argoDeleteAppResp.status = "failed";
+          argoDeleteAppResp.message = 'error with deleteing argo app';
+        }
+      } catch (e: any) {  
         if (typeof e.message === 'string') {
           isAppexist = false;
+          countinueToDeleteProject = true
           argoDeleteAppResp.status = "failed";
           argoDeleteAppResp.message = e.message;
         }
-        argoDeleteAppResp.status = "failed";
-        argoDeleteAppResp.message = 'error with deleteing argo app';
       }
-
-      let argoDeleteProjectResp: Response;
       let isAppPendingDelete: boolean = false
-      try {
-        if (isAppexist) {
-          for (let attempts = 0; attempts < argoWaitCycles; attempts++) {
-            let argoApp = await argoSvc.getArgoAppData(
+      if (isAppexist) {
+        for (let attempts = 0; attempts < argoWaitCycles; attempts++) {
+          try {
+            const argoApp = await argoSvc.getArgoAppData(
               matchedArgoInstance.url,
               matchedArgoInstance.name,
               { name: argoAppName },
@@ -270,42 +255,53 @@ export function createRouter({
             );
             isAppPendingDelete = 'metadata' in argoApp;
             if (!isAppPendingDelete) { 
-              argoDeleteAppResp.status = "sucess"
+              argoDeleteAppResp.status = "success"
               argoDeleteAppResp.message = 'application is deleted successfully'; 
               break;
             };
-            await new Promise(resolve => setTimeout(resolve, 5000));
+            // await new Promise(resolve => setTimeout(resolve, 5000));
+          } catch (e: any) { 
+            if (attempts === argoWaitCycles){
+              argoDeleteAppResp.status = "failed";
+              argoDeleteAppResp.message = "error getting argo app data";
+          }
+            continue;
           }
         }
-
-        if (isAppPendingDelete) {
+      }
+      try {
+        if (isAppPendingDelete && isAppexist) {
             argoDeleteAppResp.status = "failed"
-            argoDeleteAppResp.message = 'application pending delete';            
-        } else {
+            argoDeleteAppResp.message = 'application pending delete';
+            argoDeleteProjectResp.status = 'failed';
+            argoDeleteProjectResp.message = 'skipping project deletion due to app deletion pending';
+        } else if (countinueToDeleteProject) {
             await argoSvc.deleteProject({
               baseUrl: matchedArgoInstance.url,
               argoProjectName: argoAppName,
               argoToken: token,          
           });
-          argoDeleteProjectResp.status = "success";
+          argoDeleteProjectResp.status = 'success';
           argoDeleteProjectResp.message = 'project is deleted successfully';
-        }
+        } else {
+          argoDeleteProjectResp.status = 'failed';
+          argoDeleteProjectResp.message = 'skipping project deletion due to erro deleting argo app';
+        } 
       } catch (e: any) {
         if (typeof e.message === 'string') {
           argoDeleteProjectResp.status = "failed";
           argoDeleteProjectResp.message = e.message;
+        } else {
+          argoDeleteProjectResp.status = "failed";
+          argoDeleteProjectResp.message = 'error with deleteing argo project';
         }
-        argoDeleteProjectResp.status = "failed";
-        argoDeleteProjectResp.message = 'error with deleteing argo project';
       }
-
       return response.send({
         argoDeleteAppResp: argoDeleteAppResp,
         argoDeleteProjectResp: argoDeleteProjectResp,
       });
     },
   );
-
   router.use(errorHandler());
   return Promise.resolve(router);
 }
