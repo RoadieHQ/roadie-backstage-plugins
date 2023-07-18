ec38b2556268c0d337056747056f465aac12e7d1
Matt Benson <gudnabrsam@gmail.com>
2023-07-18 15:56:23 -0500

    correct test descriptions

--- plugins/scaffolder-actions/scaffolder-backend-module-utils/src/actions/jsonata/yaml.test.ts
+++ plugins/scaffolder-actions/scaffolder-backend-module-utils/src/actions/jsonata/yaml.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Larder Software Limited
+ * Copyright 2023 Larder Software Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { getVoidLogger } from '@backstage/backend-common';
 import { createYamlJSONataTransformAction } from './yaml';
 import { PassThrough } from 'stream';
@@ -35,7 +36,7 @@ describe('roadiehq:utils:jsonata:yaml:transform', () => {
   };
   const action = createYamlJSONataTransformAction();
 
-  it('should write file to the workspacePath with the given transformation', async () => {
+  it('should output result of having applied the given transformation', async () => {
     mock({
       'fake-tmp-dir': {
         'fake-file.yaml': 'hello: [world]',
