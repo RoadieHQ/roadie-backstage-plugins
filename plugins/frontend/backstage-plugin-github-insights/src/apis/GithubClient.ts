75b1c11a2c8e974df43841059e61fb2ebc6b41a7
Vity <vity@vity.cz>
2024-06-26 09:10:14 +0200

    Github Insights - README.MD fixes - Links and images in the Readme card are displayed as broken (#1445)
    * fixes for https://github.com/RoadieHQ/roadie-backstage-plugins/issues/1118
    
    Signed-off-by: Ladislav Vitásek <vity@vity.cz>
    
    * hot fix yarn repository URL
    
    Signed-off-by: Ladislav Vitásek <vity@vity.cz>
    
    * useless comment removed
    
    Signed-off-by: Ladislav Vitásek <vity@vity.cz>
    
    ---------
    
    Signed-off-by: Ladislav Vitásek <vity@vity.cz>

--- plugins/frontend/backstage-plugin-github-insights/src/apis/GithubClient.ts
+++ plugins/frontend/backstage-plugin-github-insights/src/apis/GithubClient.ts
@@ -17,6 +17,7 @@
 import { GithubApi } from './GithubApi';
 import { ErrorApi, OAuthApi } from '@backstage/core-plugin-api';
 import { Octokit } from '@octokit/rest';
+import parseGitUrl from 'git-url-parse';
 
 const mimeTypeMap: Record<string, string> = {
   svg: 'image/svg+xml',
@@ -62,6 +63,7 @@ export class GithubClient implements GithubApi {
     const { path, repo, owner, branch, baseUrl = defaultBaseUrl } = props;
     const token = await this.githubAuthApi.getAccessToken();
     const octokit = new Octokit({ auth: token, baseUrl });
+
     let query = 'readme';
     if (path) {
       query = `contents/${path}`;
@@ -80,25 +82,46 @@ export class GithubClient implements GithubApi {
 
     const mediaLinks = [
       ...content.matchAll(
-        /\[([^\[\]]*)\]\((.*?)(\.png|\.jpg|\.jpeg|\.gif|\.webp|\.svg)(.*)\)/gim,
+        /!\[([^\]]*)\]\(([^)]+\.(?:png|jpg|jpeg|gif|webp|svg))\)/gi,
       ),
     ].map(match => [match[2], match[3]].join(''));
 
     const media: Record<string, string> = {};
 
-    for (const href of mediaLinks) {
-      const mimeType = mimeTypeLookup(href);
-      const url = new URL(href, response.data.url);
+    const { ref, resource, protocol } = parseGitUrl(response.data.url);
 
-      if (mimeType && url.host.includes('github.com')) {
-        const requestPath = url.pathname.replace(
-          new RegExp(`/${owner}/${repo}/blob/(main|master)`),
-          `/repos/${owner}/${repo}/contents`,
-        );
+    for (const mediaLink of mediaLinks) {
+      const mimeType = mimeTypeLookup(mediaLink);
+      if (!mimeType) {
+        continue;
+      }
+
+      const linkLowerCased = mediaLink.toLocaleLowerCase('en-US');
+      const isAbsolute = linkLowerCased.startsWith('http');
+
+      let url;
+      // 1. Media link is relative to the readme file - eg. './img/myimage.png'
+      if (!isAbsolute) {
+        url = new URL(mediaLink, response.data.url);
+      } else if (linkLowerCased.includes(resource)) {
+        // 2. Media link is not referencing to an external github instance
+        // eg. https://github.com/myorg/myrepo/blob/master/32x32/gcp-logs.png
+        // we need a relative reference to create GitHub API call
+        const {
+          owner: ownerLink,
+          name: repoLink,
+          ref: refLink,
+          filepath,
+        } = parseGitUrl(mediaLink);
+
+        url = `/repos/${ownerLink}/${repoLink}/contents/${filepath}?ref=${refLink}`;
+      }
+
+      if (url) {
         try {
-          const contentResponse = await octokit.request(`GET ${requestPath}`);
+          const contentResponse = await octokit.request(`GET ${url}`);
           media[
-            href
+            mediaLink
           ] = `data:${mimeType};base64,${contentResponse.data.content.replaceAll(
             '\n',
             '',
@@ -114,10 +137,14 @@ export class GithubClient implements GithubApi {
     ].map(match => [match[2], match[3]].join(''));
 
     const links: Record<string, string> = {};
+
+    const loadFromBranch =
+      branch || ref || getRepositoryDefaultBranch(response.data.url);
+
     for (const markdownLink of markdownLinks) {
-      links[markdownLink] = `https://github.com/${owner}/${repo}/blob/${
-        branch || getRepositoryDefaultBranch(response.data.url)
-      }/${markdownLink}`;
+      links[
+        markdownLink
+      ] = `${protocol}://${resource}/${owner}/${repo}/blob/${loadFromBranch}/${markdownLink}`;
     }
     return { content, media, links };
   }
