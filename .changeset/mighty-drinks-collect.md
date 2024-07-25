ed7369138fd3ff7eb1b81d082d7da79a5651a364
Benjamin Janssens <benji.janssens@gmail.com>
2024-07-25 11:20:16 +0200

    chore(ai): add changesets; use JSON.stringify for passing a filter to the query

---
+++ .changeset/mighty-drinks-collect.md
@@ -0,0 +1,5 @@
+---
+'@roadiehq/rag-ai-storage-pgvector': patch
+---
+
+Use `JSON.stringify` for passing the filter to the query
--- plugins/backend/rag-ai-storage-pgvector/src/service/RoadiePgVectorStore.ts
+++ plugins/backend/rag-ai-storage-pgvector/src/service/RoadiePgVectorStore.ts
@@ -222,7 +222,7 @@ export class RoadiePgVectorStore implements RoadieVectorStore {
     const documents = (
       await this.client.raw(queryString, {
         embeddingString,
-        filter: JSON.stringify(filter || {}),
+        filter: JSON.stringify(filter ?? {}),
         amount,
       })
     ).rows;
