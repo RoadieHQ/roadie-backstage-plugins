efd251d97c816845b7a4ce3a9615257d48af7788
Benjamin Janssens <benji.janssens@gmail.com>
2024-08-21 16:42:37 +0200

    fix: polyfill TransformStream in tests; update lockfile

--- packages/app/src/setupTests.ts
+++ packages/app/src/setupTests.ts
@@ -1,2 +1,7 @@
 // eslint-disable-next-line notice/notice
 import '@testing-library/jest-dom';
+import { TransformStream } from 'node:stream/web';
+
+Object.defineProperties(globalThis, {
+  TransformStream: { value: TransformStream },
+});
