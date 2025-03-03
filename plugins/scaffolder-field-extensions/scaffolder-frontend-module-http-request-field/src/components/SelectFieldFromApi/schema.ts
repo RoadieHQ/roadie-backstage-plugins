59e1d88f1afb11ee50098c74ab9b4aa92322d670
irma12 <irma@roadie.io>
2025-03-03 11:52:42 +0100

    Add description

--- plugins/scaffolder-field-extensions/scaffolder-frontend-module-http-request-field/src/components/SelectFieldFromApi/schema.ts
+++ plugins/scaffolder-field-extensions/scaffolder-frontend-module-http-request-field/src/components/SelectFieldFromApi/schema.ts
@@ -26,12 +26,36 @@ export const selectFieldFromApiSchema = z.object({
       z.array(z.record(z.string(), z.string())),
     ])
     .optional(),
-  path: z.string(),
-  arraySelector: z.union([z.string(), z.array(z.string())]).optional(),
-  valueSelector: z.union([z.string(), z.array(z.string())]).optional(),
-  labelSelector: z.union([z.string(), z.array(z.string())]).optional(),
+  path: z
+    .string()
+    .describe(
+      'The Path on the Backstage API and the parameters to fetch the data for the dropdown',
+    ),
+  arraySelector: z
+    .union([z.string(), z.array(z.string())])
+    .optional()
+    .describe(
+      'This selects the array element from the API fetch response. It finds the array with the name kind',
+    ),
+  valueSelector: z
+    .union([z.string(), z.array(z.string())])
+    .optional()
+    .describe(
+      'This selects the field in the array to use for the value of each select item. If its not specified it will use the value of the item directly.',
+    ),
+  labelSelector: z
+    .union([z.string(), z.array(z.string())])
+    .optional()
+    .describe(
+      'This selects the field in the array to use for the label of each select item',
+    ),
   title: z.string().optional(),
-  placeholder: z.string().optional(),
+  placeholder: z
+    .string()
+    .optional()
+    .describe(
+      'Renders the provided text as a placeholder value into the select box',
+    ),
   description: z.string().optional(),
 });
 
