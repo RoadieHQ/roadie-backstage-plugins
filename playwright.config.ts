e83fedd8bf41626131dddcb3ae320ad5ee84afd4
Kiss Miklos <miklos@roadie.io>
2025-07-18 08:26:54 +0200

    add back steps
    Signed-off-by: Kiss Miklos <miklos@roadie.io>

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,8 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 0 : 0,
-  workers: process.env.CI ? 1 : 3,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : 3,
   reporter: 'html',
   testMatch: '**/e2e/**/*.test.ts',
   testIgnore: ['**/src/**', '**/node_modules/**', '**/cypress/**'],
