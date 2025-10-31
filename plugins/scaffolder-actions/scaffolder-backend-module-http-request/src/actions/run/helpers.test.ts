73ec5b01b153253c8ea45e4aac187e0a8f849c8c
punkle <brian@roadie.io>
2025-10-31 10:00:54 +0000

    remove use of backend common from scaffolder actions plugins

--- plugins/scaffolder-actions/scaffolder-backend-module-http-request/src/actions/run/helpers.test.ts
+++ plugins/scaffolder-actions/scaffolder-backend-module-http-request/src/actions/run/helpers.test.ts
@@ -20,9 +20,8 @@ import {
   getPluginId,
 } from './helpers';
 import { HttpOptions } from './types';
-import { getRootLogger } from '@backstage/backend-common';
 import { Writable } from 'stream';
-import * as winston from 'winston';
+import { createLogger, transports } from 'winston';
 import { UrlPatternDiscovery } from '@backstage/core-app-api';
 
 const mockBaseUrl = 'http://backstage.tests/api';
@@ -51,8 +50,8 @@ logStream._write = (chunk, _encoding, next) => {
   logOutput = logOutput += chunk.toString();
   next();
 };
-const streamTransport = new winston.transports.Stream({ stream: logStream });
-const logger = getRootLogger();
+const streamTransport = new transports.Stream({ stream: logStream });
+const logger = createLogger();
 logger.add(streamTransport);
 
 jest.mock('cross-fetch');
