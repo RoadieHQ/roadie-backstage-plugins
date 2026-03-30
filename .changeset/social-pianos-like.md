1f93f71a047447da85bfc3e57af3313a94ab815d
Robin Hilton <22367rh@gmail.com>
2026-03-31 12:15:13 +1300

    feat(backstage-plugin-github-pull-requests) add registration of API for new frontend

---
+++ .changeset/social-pianos-like.md
@@ -0,0 +1,5 @@
+---
+'@roadiehq/backstage-plugin-github-pull-requests': minor
+---
+
+Add compatibility with Backstage New Frontend
---
+++ plugins/frontend/backstage-plugin-github-pull-requests/src/alpha.tsx
@@ -0,0 +1,20 @@
+import { ApiBlueprint, configApiRef, createFrontendPlugin } from '@backstage/frontend-plugin-api';
+import { scmAuthApiRef } from '@backstage/integration-react';
+import { githubPullRequestsApiRef, GithubPullRequestsClient } from './api';
+
+const githubPullRequestsApi = ApiBlueprint.make({
+  params: defineParams =>
+    defineParams({
+      api: githubPullRequestsApiRef,
+      deps: { 
+	    configApi: configApiRef, 
+		scmAuthApi: scmAuthApiRef 
+	  },
+      factory: ({ configApi, scmAuthApi }) => new GithubPullRequestsClient({ configApi, scmAuthApi }),
+    }),
+});
+
+export default createFrontendPlugin({
+  pluginId: 'github-pull-requests',
+  extensions: [githubPullRequestsApi],
+});
